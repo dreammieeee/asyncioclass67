7f6380351f5e262240314224ed224b9cc7ec18fd
dreammieeee <phonchanan.ribruamsup@gmail.com>
2024-06-19 14:18:48 +0700

    submit07 assingment

--- assignment1/thread07.py
+++ assignment1/thread07.py
@@ -1,4 +1,4 @@
-# Using a ThreadPoolExecutor
+#Using a ThreadPoolExecutor
 
 import concurrent.futures
 import logging
@@ -10,6 +10,7 @@ def thread_function(name):
     logging.info("Thread %s: finishing", name)
 
 if __name__ == "__main__":
+   
     format = "%(asctime)s: %(message)s"
     logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
     with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
