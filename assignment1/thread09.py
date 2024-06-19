3962225286637fea29cc4e634bc6370cb5ead859
dreammieeee <phonchanan.ribruamsup@gmail.com>
2024-06-19 14:11:39 +0700

    submit09 assingment

--- assignment1/thread09.py
+++ assignment1/thread09.py
@@ -2,27 +2,28 @@
 import concurrent.futures
 import logging
 import threading 
-
+import time
 
 class FakeDatabase:
-    def init(self):
+    def __init__(self):
         self.value = 0
         self.lock = threading.Lock()
 
-    def lockedupdate(self,name):
+    def locked_update(self, name):
         logging.info("Thread %s: starting update", name)
-        logging.debug("Thread $s about to lock", name)
-        with self.lock:
+        logging.debug("Thread %s about to lock", name)
+        with self._lock:
             logging.debug("thread %s has lock", name)
-            localcopy =+1
+            local_copy = self.value
+            local_copy =+1
             time.sleep(0.1)
-            self.value = localcopy
+            self.value = local_copy
             logging.dubug("Thread %s about to release lock", name)
         logging.debug("Thread %s after release", name)
         logging.info("Thread %s: finishing update", name)
 
-if __name__ == "__main":
-    format ="%s(asctime)s: %(message)s"
+if __name__ == "__main__":
+    format ="%(asctime)s: %(message)s"
     logging.basicConfig(format=format, level=logging.DEBUG, datefmt="%H:%M:%S")
     #logging.getLogger().setLevel(logging.debug)
 
@@ -33,4 +34,5 @@ if __name__ == "__main":
     with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
         for index in range(2):
             executor.submit(database.locked_update, index)
-    logging.info("TEst locked update. ENding Value is%d", database.value)
+    logging.info("TEst locked update. ENding Value is %d", database.value)
+
